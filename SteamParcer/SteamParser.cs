8844139e78e8c7c001013c3b602c3e82f8bb80e6
Roman <roman-maslov423@yandex.ru>
2022-10-27 23:30:34 +0300

    beta 0.2 (peredelat price)

--- SteamParcer/SteamParser.cs
+++ SteamParcer/SteamParser.cs
@@ -5,31 +5,34 @@ using System.Net;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-
+using Magazine.Models;
 
 namespace SteamParser
 {
 
     class SteamParser
     {
-
+        
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Green;
 
             List<string> urls = new List<string>();
+            List<Game> games = new List<Game>();
             urls = getUrls("https://store.steampowered.com/search/?os=win&hidef2p=1&filter=popularnew");
-            //dsf
-            //foreach (string url in urls)
-            //{
-            //    Parse(url);
-            //}
+            
+            foreach (string url in urls)
+            { 
+                games.Add(Parse(url));
+            }
+           
         }
         
-        public static object Parse(String url)
+        public static Game Parse(String url)
         {
             try
             {
+               Game game = new Game();
                 using (HttpClientHandler hdl = new HttpClientHandler { AutomaticDecompression = System.Net.DecompressionMethods.Deflate | DecompressionMethods.GZip | DecompressionMethods.None })
                 {
                     using (var client = new HttpClient(hdl))
@@ -47,27 +50,35 @@ namespace SteamParser
                                     var img = doc.DocumentNode.SelectSingleNode(".//img[@class='game_header_image_full']").GetAttributeValue("src", "");
                                    
                                     
-                                    var imgUrl = img.ToString();
-                                    var title = doc.DocumentNode.SelectSingleNode(".//div[@class='apphub_AppName']").GetDirectInnerText();
-                                    var price = doc.DocumentNode.SelectSingleNode(".//div[@class='discount_original_price']").GetDirectInnerText();
-                                    
+                                    game.imgSrc = img.ToString();
+                                    game.name = doc.DocumentNode.SelectSingleNode(".//div[@class='apphub_AppName']").GetDirectInnerText();
+                                    if( doc.DocumentNode.SelectSingleNode(".//div[@class='game_purchase_price price']").GetDirectInnerText() == null)
+                                    {
+                                        game.price = doc.DocumentNode.SelectSingleNode(".//div[@class='discount_original_price']").GetDirectInnerText().Trim();
+                                    }
+                                    else
+                                    {
+                                        game.price = doc.DocumentNode.SelectSingleNode(".//div[@class='game_purchase_price price']").GetDirectInnerText().Trim();
+                                    }
+
+
                                     var allTags = doc.DocumentNode.SelectNodes(".//div[@class='glance_tags popular_tags']//a[@class='app_tag']");
-                                    List<string> tags = new List<string>();
+                                    List<string> tags = new List<string>(3);
                                     foreach(var tag in allTags)
                                     {
                                         tags.Add(tag.InnerText.Trim());
-                                        
                                     }
-                                    var describe = doc.DocumentNode.SelectSingleNode(".//div[@class='game_description_snippet']").GetDirectInnerText().Trim();
-                                    Console.WriteLine(describe);
-                                    
+                                    game.describe = doc.DocumentNode.SelectSingleNode(".//div[@class='game_description_snippet']").GetDirectInnerText().Trim();
+                                    game.tags = tags;
+                                    game.Print();
+                                    return game;
                                 }
                             }
                         }
                     }
                 }
             }
-            catch(Exception ex) { Console.WriteLine(ex.Message); }
+            catch(Exception ex) { }
             return null;
         }
 
@@ -96,14 +107,7 @@ namespace SteamParser
                                             result.Add(bebra.GetAttributeValue("href", ""));
                                            
                                         }
-                                    int i = 0;
-                                    foreach(var url1 in result)
-                                    {
-                                        Console.WriteLine(url1);
-                                        i++;
-                                    }
-                                    Console.WriteLine(i);
-
+                                 
                                 }
 
                             }
